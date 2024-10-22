72fd55b018873e6cce8efd8fb3ef0cc59f82b95d
ezequiel <ezequielcruz018@gmail.com>
2024-10-22 11:48:39 -0400

    feat: apply typing and improve image rendering
    - Added type definitions to enhance code quality.
    - Utilized the `image` library for better image rendering.
    - Updated the structure of the map to display a message when no items are found.

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -1,28 +1,43 @@
+import Image from "next/image";
 import styles from "../styles/ProductList.module.css";
 import { ProductsUser } from "@/app/types/products";
 
-export default function ProductList({ products, onProductClick }) {
+type ProductListProps = {
+  products: ProductsUser[];
+  onProductClick: (product: ProductsUser) => void;
+};
+
+export default function ProductList({
+  products,
+  onProductClick,
+}: ProductListProps) {
   return (
     <div className={styles.grid}>
-      {products?.map((product: ProductsUser) => (
-        <div
-          key={product.id}
-          className={styles.card}
-          onClick={() => onProductClick(product)}
-        >
-          <img
-            src={product.image}
-            alt={product.name}
-            className={styles.image}
-          />
-          <div className={styles.cardContent}>
-            <h2 className={styles.productName}>{product.name}</h2>
-            <p className={styles.description}>
-              {product.description.slice(0, 60)}...
-            </p>
+      {products.length > 0 ? (
+        products?.map((product: ProductsUser) => (
+          <div
+            key={product.id}
+            className={styles.card}
+            onClick={() => onProductClick(product)}
+          >
+            <Image
+              src={product.image}
+              alt={product.name}
+              width={500}
+              height={500}
+              className={styles.image}
+            />
+            <div className={styles.cardContent}>
+              <h2 className={styles.productName}>{product.name}</h2>
+              <p className={styles.description}>
+                {product.description.slice(0, 60)}...
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <p>No hay productos disponibles</p>
+      )}
     </div>
   );
 }
